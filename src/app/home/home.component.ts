fb68488fddcbb1dbada51c8c8ea66a231f324823
Lupea Mihai <mihai.lupea@gmail.com>
2019-04-06 23:12:43 +0300

    alerts service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { VoiceListenerService } from '../voiceAssistant/voice-listener.service';
 import { ChattingService } from '../voiceAssistant/chatting.service';
+import { FaceMatcher } from 'face-api.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild("video")
+  public video: ElementRef;
   constructor(private chattingService: ChattingService, private voiceListener: VoiceListenerService) { }
 
   ngOnInit() {
@@ -16,4 +18,13 @@ export class HomeComponent implements OnInit {
     this.voiceListener.recognisedCommand.subscribe(e => console.log(e));
   }
 
+  public ngAfterViewInit() {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        console.log(stream);
+          this.video.nativeElement.src = window.URL.createObjectURL(stream);
+          this.video.nativeElement.play();
+      });
+    }
+  }
 }
---
+++ src/app/services/alert.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Alert } from '../models/alert';
+import { Observable, Subject } from 'rxjs';
+import { isNullOrUndefined } from 'util';
+import { map } from 'rxjs/operators';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+  constructor(private _afDb: AngularFirestore) { }
+  
+  private _alertaEntriesObservable: Observable<Alert[]>;
+  private _alertaEntriesSubject: Subject<Alert[]>;
+
+  public addAlert(oraData: Alert): Promise<any> {
+    return this._afDb.collection('Alerte').add(
+        { ...oraData, timestamp: firebase.database.ServerValue.TIMESTAMP }
+      );
+  }
+
+  public getAlerte(grupa?: string): Observable<Alert[]> {
+    if (isNullOrUndefined(this._alertaEntriesObservable)) {
+      this._alertaEntriesObservable = this._afDb.collection('Alerte').snapshotChanges().pipe(map(alerte => alerte.map(alerta => alerta.payload.doc.data() as Alert)));
+      this._alertaEntriesObservable.subscribe(ore => this._alertaEntriesSubject.next(ore));
+    }
+
+    return this._alertaEntriesSubject.asObservable()
+      .pipe(
+        map(alerte => {
+          let alerteFinal: Alert[]
+          alerteFinal = alerte.filter(alerta => {
+            return isNullOrUndefined(grupa) || alerta.grupa == grupa;
+          })
+          return alerteFinal;
+        })
+      );
+  }
+}
