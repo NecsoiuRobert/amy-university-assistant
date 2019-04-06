609c19bffb0ba159874436e18308db901a4d4ac1
ParaschivGeorge <georgegmtt@gmail.com>
2019-04-06 22:59:39 +0300

    alert comp without services

--- src/app/alerts/alerts.component.ts
+++ src/app/alerts/alerts.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Alert } from '../models/alert';
+import { AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
   styleUrls: ['./alerts.component.scss']
 })
-export class AlertsComponent implements OnInit {
+export class AlertsComponent implements OnInit, AfterViewInit {
+
+  alertForm = new FormGroup({
+    title: new FormControl(null, Validators.required),
+    announce: new FormControl(null, Validators.required),
+    grupa: new FormControl(null), // mandatory la seminar / laborator, null la curs
+  });
+
+  alerts: Alert[] = [];
+  grupe = ['313CC', '335CA', '343C5'];
 
   constructor() { }
 
   ngOnInit() {
+    // TODO get alerts
+  }
+  
+  ngAfterViewInit() {
+    $('select').formSelect();
+  }
+
+  onSubmit() {
+    console.log(this.alertForm);
+    if (this.alertForm.valid) {
+      const response = this.alertForm.value as Alert;
+      console.log(response);
+      // TODO add alert
+    }
   }
 
 }
