46730ef7631231d3bba90afae6eed5ebe4c9623b
ParaschivGeorge <georgegmtt@gmail.com>
2019-04-06 22:02:19 +0300

    orar-edit

--- src/app/admin/orar-edit/orar-edit.component.ts
+++ src/app/admin/orar-edit/orar-edit.component.ts
@@ -1,38 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { constructor } from 'q';
+declare var $: any;
 
 @Component({
   selector: 'app-orar-edit',
   templateUrl: './orar-edit.component.html',
   styleUrls: ['./orar-edit.component.sass']
 })
-export class OrarEditComponent implements OnInit {
+export class OrarEditComponent implements OnInit, AfterViewInit {
 
   orarEntryForm = new FormGroup({
     title: new FormControl(null, Validators.required),
     acronim: new FormControl(null, Validators.required),
     description: new FormControl(null, Validators.required),
     professor: new FormControl(null, Validators.required),
+    room: new FormControl(null, Validators.required),
+    grupa: new FormControl(''), // mandatory la seminar / laborator, null la curs
     day: new FormControl(null, Validators.required),
     hour: new FormControl(null, Validators.required),
     duration: new FormControl(null, Validators.required),
-    room: new FormControl(null, Validators.required),
-    type: new FormControl(null, Validators.required), //curs, seminar, laborator
-    grupa: new FormControl('') // mandatory la seminar / laborator, null la curs 
+    type: new FormControl(null, Validators.required) //curs, seminar, laborator
   });
 
-  days: ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata', 'Duminica'];
-  grupe: ['313CC', '335CA', '343C5'];
+  days = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata', 'Duminica'];
+  grupe = ['313CC', '335CA', '343C5'];
+  types = ['Curs', 'Seminar', 'Laborator'];
+  rooms = ['EC004', 'EC105', 'EC101', 'EC102'];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    $('select').formSelect();
+  }
+
   getAvailableHours() {
     // TODO: make this smart
-    return [8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19];
+    return [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
   }
 
   getAvailableDurations() {
@@ -40,6 +47,11 @@ export class OrarEditComponent implements OnInit {
     return [1, 2, 3];
   }
 
-  
+  onSubmit() {
+    console.log(this.orarEntryForm);
+    if (this.orarEntryForm.valid) {
+      console.log('valid');
+    }
+  }
 
 }
